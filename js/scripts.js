e65aa5842276b86ab9336d4b3268e9104aaee610
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-01-27 13:03:54 -0800

    correct format of money and also implements spec 2

--- js/scripts.js
+++ js/scripts.js
@@ -32,6 +32,19 @@ Pizza.prototype.calculateCost = function(sizePrices, toppingDefaultPrice) {
  // Front End Section
 /////////////////////
 
+// From: http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+Number.prototype.formatMoney = function(c, d, t){
+var n = this,
+    c = isNaN(c = Math.abs(c)) ? 2 : c,
+    d = d == undefined ? "." : d,
+    t = t == undefined ? "," : t,
+    s = n < 0 ? "-" : "",
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+
 $(document).ready(function() {
 
   $("form").submit(function(event) {
@@ -46,7 +59,7 @@ $(document).ready(function() {
     var orderPizza = new Pizza(pizzaSize, pizzaToppings);
     var pizzaPrice =  orderPizza.calculateCost(pizzaSizePrices, pizzaToppingDefaultPrice);
 
-    $("#outputSection").children().first().text(pizzaPrice);
+    $("#outputSection").children().first().text("$" + pizzaPrice.formatMoney(2));
     $("#outputSection").fadeOut(100).fadeIn(100);
   });
 
