7a8162a62c2d9ed4aafd9b807d39ca42b3baa456
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-01-27 10:19:01 -0800

    add front end capability to test first spec

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,18 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
+    // Capture name value pairs of selected pizza options
+    var pizzaOptions = [];
+    $(".pizzaOption input:checked").each(function() {
+      pizzaOptions.push({
+        name: $(this).attr("name"),
+        value: $(this).val()
+      })
+    });
+
+    var pizzaPrice =  pizzaOptions.length?pizzaOptions[0].value:"Please choose size";
+
+    $("#outputSection").children().first().text(pizzaPrice);
     $("#outputSection").fadeOut(100).fadeIn(100);
   });
 
