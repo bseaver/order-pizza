55cecb81ca049834c4e86d902920cedc62c8cc48
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-01-27 11:21:21 -0800

    implement spec to set size and get price

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
   ////////////////////
  // Back End Section
 ////////////////////
+var pizzaSizePrices = [
+  [11, 5.00],
+  [18, 9.00],
+];
 
+function Pizza(size) {
+  this.size = size;
+}
+
+Pizza.prototype.calculateCost = function(sizePrices) {
+  for (var i = 0; i < sizePrices.length; i++) {
+    if (sizePrices[i][0] == this.size) {
+      return sizePrices[i][1];
+    }
+  }
+};
 
   /////////////////////
  // Front End Section
 /////////////////////
+
 $(document).ready(function() {
 
   $("form").submit(function(event) {
     event.preventDefault();
 
-    // Capture name value pairs of selected pizza options
-    var pizzaOptions = [];
-    $(".pizzaOption input:checked").each(function() {
-      pizzaOptions.push({
-        name: $(this).attr("name"),
-        value: $(this).val()
-      })
-    });
+    var pizzaSize = $("#pizzaSize input:checked").val();
 
-    var pizzaPrice =  pizzaOptions.length?pizzaOptions[0].value:"Please choose size";
+    var orderPizza = new Pizza(pizzaSize);
+    var pizzaPrice =  orderPizza.calculateCost(pizzaSizePrices)
 
     $("#outputSection").children().first().text(pizzaPrice);
     $("#outputSection").fadeOut(100).fadeIn(100);
